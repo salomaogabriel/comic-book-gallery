180f29059916dd9ca7f8f948f43c9c9615e6b1fc
Gabriel Salomão Coelho da Costa <gabrielcoelhodacosta2002@gmail.com>
2022-03-28 21:35:28 +0200

    Add Comic Book and Artists Models

---
+++ src/ComicBookGallery/Models/ComicBookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBookModel
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHTML { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
