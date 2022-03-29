9d01b82e5ef673105059b51a48a74206d9e52023
Gabriel Salomão Coelho da Costa <gabrielcoelhodacosta2002@gmail.com>
2022-03-29 17:47:04 +0200

    Add Comic book repository

--- src/ComicBookGallery/Controllers/HomeController.cs
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
@@ -23,23 +24,9 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            var comicBook = new ComicBookModel()
-            { 
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name="Dan Slott",Role="Script"},
-                    new Artist() {Name="Humberto Ramos",Role="Pencils"},
-                    new Artist() {Name="Victor Olazaba",Role="Inks"},
-                    new Artist() {Name="Edgar Delgado",Role="Colors"},
-                    new Artist() {Name="Chris Eliopoulos",Role="Letters"},
-                }
-        };
-
             
-            return View("Detail",comicBook);
+            
+            return View("Detail",);
         }
     }
 }
