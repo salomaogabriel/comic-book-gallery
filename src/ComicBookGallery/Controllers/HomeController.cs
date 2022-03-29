a7be28314e94d3d31e387bfe91829c2e510e0c67
Gabriel Salomão Coelho da Costa <gabrielcoelhodacosta2002@gmail.com>
2022-03-29 17:09:56 +0200

    Update comic book detail view to be strongly type

--- src/ComicBookGallery/Controllers/HomeController.cs
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -23,20 +23,23 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
+            var comicBook = new ComicBookModel()
+            { 
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name="Dan Slott",Role="Script"},
+                    new Artist() {Name="Humberto Ramos",Role="Pencils"},
+                    new Artist() {Name="Victor Olazaba",Role="Inks"},
+                    new Artist() {Name="Edgar Delgado",Role="Colors"},
+                    new Artist() {Name="Chris Eliopoulos",Role="Letters"},
+                }
+        };
 
-
-            ViewBag.SeriesTitles = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-            return View("Detail");
+            
+            return View("Detail",comicBook);
         }
     }
 }
